31335346ed6666ffcc293af9a43254503d1874fa
Augusto Leal <aleal4@gmail.com>
2023-01-12 14:08:07 -0700

    setting up express

--- today/src/index.js
+++ today/src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 });
 
+app.get('/todos', (req, res) => {
+  res.send('get todos!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

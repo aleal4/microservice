a3702f652d031f4e80736226bdd18dff5c4ca089
Augusto Leal <aleal4@gmail.com>
2023-01-12 14:13:00 -0700

    back to basics

--- today/src/index.js
+++ today/src/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const axios = require('axios')
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 });
 
 app.get('/todos', (req, res) => {
-  res.send('get todos!')
+  axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  res.send('1234')
 })
 
 app.listen(port, () => {
